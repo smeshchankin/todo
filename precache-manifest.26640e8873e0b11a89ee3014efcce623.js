62adda0f98030946de1fe5bacfde9ea12cb3f225
Sergey Meshchankin <smeshchankin@gmail.com>
2020-05-30 00:54:57 +0300

    Updates

---
+++ precache-manifest.26640e8873e0b11a89ee3014efcce623.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79843ed9bea7148308f1606ae2adf5e6",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "b62b8a7071e68b270122",
+    "url": "/todo/static/js/2.87eb8b5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.87eb8b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281d0839aade14c70805",
+    "url": "/todo/static/js/main.45d7d80c.chunk.js"
+  },
+  {
+    "revision": "42e23f9118876525bea8",
+    "url": "/todo/static/js/runtime-main.0d77e128.js"
+  }
+]);
