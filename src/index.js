fe5711676ca2882b00f0e4463efabc97dc990649
Sergey Meshchankin <smeshchankin@gmail.com>
2020-05-31 21:38:24 +0300

    [TD-001] React element: app structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactoDOM from 'react-dom';
 
-const elem = <h1>Todo App</h1>;
+const elem = (
+    <div>
+        <h1>Todo List</h1>
+        <div class='filter-group'>
+            <input placeholder="Search" />
+            <button>All</button>
+            <button>Active</button>
+            <button>Done</button>
+        </div>
+        <ul>
+            <li>Learn React</li>
+            <li>Create React App</li>
+            <li>Deploy App on GitHub Pages</li>
+        </ul>
+        <div id="add-group">
+            <input placeholder="Add item" />
+            <button>+</button>
+        </div>
+    </div>
+);
+const root = document.getElementById('root');
+console.log(elem);
 
-ReactoDOM.render(elem, document.getElementById('root'));
+ReactoDOM.render(elem, root);
