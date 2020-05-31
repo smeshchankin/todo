5b1458514a0cdb4b263b4a5aea0a8b063d2a092e
Sergey Meshchankin <smeshchankin@gmail.com>
2020-05-31 22:40:21 +0300

    [TD-001][FIX] React element: use className

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactoDOM from 'react-dom';
 const elem = (
     <div>
         <h1>Todo List</h1>
-        <div class='filter-group'>
+        <div className='filter-group'>
             <input placeholder="Search" />
             <button>All</button>
             <button>Active</button>
@@ -15,7 +15,7 @@ const elem = (
             <li>Create React App</li>
             <li>Deploy App on GitHub Pages</li>
         </ul>
-        <div id="add-group">
+        <div className="add-group">
             <input placeholder="Add item" />
             <button>+</button>
         </div>
