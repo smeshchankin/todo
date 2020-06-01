6418059772bcbfd543faa8e71cc32149c1731917
Sergey Meshchankin <smeshchankin@gmail.com>
2020-06-01 22:38:16 +0300

    [TD-002] React components: extract TodoItem

---
+++ src/components/todo-item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoItem = () => {
+    return <span className="todo-item">Task name</span>;
+};
+
+export default TodoItem;
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import TodoItem from './todo-item';
+
 const TodoList = () => {
     return (
         <ul className="todo-list">
             <li className="todo-item">Learn React</li>
             <li className="todo-item">Create React App</li>
             <li className="todo-item">Deploy App on GitHub Pages</li>
+            <li><TodoItem /></li>
         </ul>
     );
 };
