aa24b808a7387d870887ab11658d8ee1ca260b84
Sergey Meshchankin <smeshchankin@gmail.com>
2020-06-04 23:59:37 +0300

    [TD-002] React components: TodoItem class

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './todo-item.css';
 
-const TodoItem = ({ text, important }) => {
-    const classes = "todo-item" + (important ? ' important' : '');
-    return (
-        <div className="item">
-            <span className={classes}>{ text }</span>
-            <div className="item-buttons">
-                <button>X</button>
-                <button>!</button>
+export default class TodoItem extends Component {
+    render() {
+        const { text, important } = this.props
+        const classes = "todo-item" + (important ? ' important' : '');
+        return (
+            <div className="item">
+                <span className={classes}>{ text }</span>
+                <div className="item-buttons">
+                    <button>X</button>
+                    <button>!</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
-
-export default TodoItem;
