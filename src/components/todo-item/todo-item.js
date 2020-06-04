d5d6aa4ed4dc43ba47d89c008e8213bab626beb0
Sergey Meshchankin <smeshchankin@gmail.com>
2020-06-05 00:27:48 +0300

    [TD-003] Handle event: arrow function in constructor

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -3,15 +3,18 @@ import React, { Component } from 'react';
 import './todo-item.css';
 
 export default class TodoItem extends Component {
-    onClick() {
-        console.log(`Done: ${this.props.text}`)
+    constructor() {
+        super();
+        this.onClick = () => {
+            console.log(`Done: ${this.props.text}`)
+        };
     }
 
     render() {
         const { text, important } = this.props
         const classes = "todo-item" + (important ? ' important' : '');
         return (
-            <div className="item" onClick={ this.onClick.bind(this) }>
+            <div className="item" onClick={ this.onClick }>
                 <span className={classes}>{ text }</span>
                 <div className="item-buttons">
                     <button>X</button>
