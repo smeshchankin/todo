ee948512c303564a83499d449d05e31ea1e69220
Sergey Meshchankin <smeshchankin@gmail.com>
2020-06-04 23:47:40 +0300

    [TD-002] React components: header with counters

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -3,7 +3,12 @@ import React from 'react';
 import './app-header.css';
 
 const AppHeader = () => {
-    return <h1>Todo List</h1>;
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <span className="counters">{1} more to do, {3} done</span>
+        </div>
+    );
 };
 
 export default AppHeader;
