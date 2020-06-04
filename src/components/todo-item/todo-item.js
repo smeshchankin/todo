d6829fb377f9fd5b91257df2c28891eeb2fd7b70
Sergey Meshchankin <smeshchankin@gmail.com>
2020-06-05 00:25:22 +0300

    [TD-003] Handle event: with bind(this)

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 import './todo-item.css';
 
 export default class TodoItem extends Component {
+    onClick() {
+        console.log(`Done: ${this.props.text}`)
+    }
+
     render() {
         const { text, important } = this.props
         const classes = "todo-item" + (important ? ' important' : '');
         return (
-            <div className="item">
+            <div className="item" onClick={ this.onClick.bind(this) }>
                 <span className={classes}>{ text }</span>
                 <div className="item-buttons">
                     <button>X</button>
