0a03cd7fff6ed43dbf7230eced964e73f042ee6d
Sergey Meshchankin <smeshchankin@gmail.com>
2020-06-05 00:38:40 +0300

    [TD-004] State: set done for item by click

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -3,13 +3,23 @@ import React, { Component } from 'react';
 import './todo-item.css';
 
 export default class TodoItem extends Component {
+    state = {
+        done: false
+    };
+
     onClick = () => {
-        console.log(`Done: ${this.props.text}`)
+        this.setState({
+            done: true
+        });
     };
 
     render() {
-        const { text, important } = this.props
-        const classes = "todo-item" + (important ? ' important' : '');
+        const { text, important } = this.props;
+        const { done } = this.state;
+        const classes = "todo-item" +
+            (important ? ' important' : '') +
+            (done ? ' done': '');
+
         return (
             <div className="item" onClick={ this.onClick }>
                 <span className={classes}>{ text }</span>
