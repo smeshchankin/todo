6f7a0c03fff001e393f2e59d70d0eca80136718a
Sergey Meshchankin <smeshchankin@gmail.com>
2020-06-04 23:11:02 +0300

    [TD-002] React components: TodoItem buttons

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -4,7 +4,15 @@ import './todo-item.css';
 
 const TodoItem = ({ text, important }) => {
     const classes = "todo-item" + (important ? ' important' : '');
-    return <span className={classes}>{ text }</span>;
+    return (
+        <div className="item">
+            <span className={classes}>{ text }</span>
+            <div className="item-buttons">
+                <button>X</button>
+                <button>!</button>
+            </div>
+        </div>
+    );
 };
 
 export default TodoItem;
