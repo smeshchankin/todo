531041de0c07d198f7ab06e6df5b8273a035088d
Sergey Meshchankin <smeshchankin@gmail.com>
2020-06-08 22:31:25 +0300

    [TD-004] State: add item from input value

--- src/components/insert-panel/insert-panel.js
+++ src/components/insert-panel/insert-panel.js
@@ -3,11 +3,25 @@ import React, { Component } from 'react';
 import './insert-panel.css';
 
 export default class InsertPanel extends Component {
+    state= {
+        task: ''
+    };
+
+    onChange = (e) => {
+        this.setState({
+            task: e.target.value
+        });
+    };
+
     render() {
         return (
             <form className="add-group">
-                <input placeholder="Add item" className="add-group__input" />
-                <button type="button" className="add-group__button" onClick={() => this.props.onInsert('Dummy task') }>+</button>
+                <input type="text"
+                    className="add-group__input"
+                    onChange={ this.onChange }
+                    placeholder="Add item"
+                />
+                <button type="button" className="add-group__button" onClick={() => this.props.onInsert(this.state.task) }>+</button>
             </form>
         );
     }
