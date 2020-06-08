e78ea024fe09a1ec83fef4f913f19d5abb7a4cd8
Sergey Meshchankin <smeshchankin@gmail.com>
2020-06-08 22:35:19 +0300

    [TD-004] State: add item via form submit

--- src/components/insert-panel/insert-panel.js
+++ src/components/insert-panel/insert-panel.js
@@ -13,15 +13,20 @@ export default class InsertPanel extends Component {
         });
     };
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onInsert(this.state.task);
+    };
+
     render() {
         return (
-            <form className="add-group">
+            <form className="add-group" onSubmit={ this.onSubmit }>
                 <input type="text"
                     className="add-group__input"
                     onChange={ this.onChange }
                     placeholder="Add item"
                 />
-                <button type="button" className="add-group__button" onClick={() => this.props.onInsert(this.state.task) }>+</button>
+                <button type="submit" className="add-group__button">+</button>
             </form>
         );
     }
